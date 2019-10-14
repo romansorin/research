6fd652128076f03f8ec0f5d7ff228e9814d54d15
Roman Sorin <988244@students.mentorschools.org>
2019-10-14 10:13:43 -0400

    superclass Reference

--- ieee.py
+++ ieee.py
@@ -10,7 +10,40 @@ info = {
 }
 
 
-class Journal:
+class Reference:
+    def __init__(self, info):
+        self.info = info
+        self.reference = []
+
+    def cite(self):
+        for author in self.info['authors']:
+            self.reference.append(author)
+        return self.reference
+
+
+class Journal(Reference):
+    def __init__(self, info):
+        self.info = info
+        self.reference = []
+
+    def cite(self):
+        for author in self.info['authors']:
+            self.reference.append(author)
+        return self.reference
+
+
+class Book(Reference):
+    def __init__(self, info):
+        self.info = info
+        self.reference = []
+
+    def cite(self):
+        for author in self.info['authors']:
+            self.reference.append(author)
+        return self.reference
+
+
+class Article(Reference):
     def __init__(self, info):
         self.info = info
         self.reference = []
