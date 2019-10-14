69f02fb2cbf9d2311ac14f9afa252352441b7921
Roman Sorin <988244@students.mentorschools.org>
2019-10-14 10:10:44 -0400

    Journal class (initial)

--- ieee.py
+++ ieee.py
@@ -1,5 +1,33 @@
-types = ['article', 'journal', 'book']
-author = 'f. last'
+
+types = {
+    'JOURNAL': 'journal',
+    'BOOK': 'book',
+    'ARTICLE': 'article'
+}
+
+info = {
+    'authors': ['R. Sorin', 'R. M. Sorin', 'dsdsd']
+}
 
 
-def generate_citation:
+class Journal:
+    def __init__(self, info):
+        self.info = info
+        self.reference = []
+
+    def cite(self):
+        for author in self.info['authors']:
+            self.reference.append(author)
+        return self.reference
+
+
+def generate_citation(type, info):
+    if type == types['JOURNAL']:
+        return Journal(info).cite()
+    elif type == types['BOOK']:
+        return Book(info).cite()
+    elif type == types['ARTICLE']:
+        return Article(info).cite()
+
+
+print(generate_citation('journal'))
