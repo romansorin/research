b6315d337d3837e1dded254ce1304ed16f1ec2da
Roman Sorin <roman@romansorin.com>
2019-12-17 20:47:43 -0500

    remove old py classes

--- Reference.py
+++
@@ -1,9 +0,0 @@
-class Reference:
-    def __init__(self, info):
-        self.info = info
-        self.reference = []
-
-    def cite(self):
-        for author in self.info['authors']:
-            self.reference.append(author)
-        return self.reference
